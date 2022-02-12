c6bc0d74614619966c1d05edf7f184c758561ea7
elinda <ela.pospechova@gmail.com>
2022-02-12 01:42:26 +0100

    menší úpravy, přidána dokumentace

--- priklad122_generalizace.py
+++ priklad122_generalizace.py
@@ -1,8 +1,9 @@
 try:
-    import matplotlib.pyplot as plt
-    from math import sqrt
     import json
     import sys
+    from math import sqrt
+    import matplotlib.pyplot as plt
+    
 except ImportError:
     sys.exit("An error occured while importing a library. Check if the required libraries are installed correctly.")
 
@@ -14,7 +15,7 @@ def pythagoras(x1, y1, x2, y2):
 
 
 def heron(a, b, c):
-    # Computes area of a triangle defined by lenghts of its sides using Herons' formula.
+    # Computes area of a triangle defined by lengths of its sides using Herons' formula.
     s = (a+b+c)/2
     area = sqrt(s*(s-a)*(s-b)*(s-c))
     return area
@@ -22,21 +23,21 @@ def heron(a, b, c):
 
 def visvalingam_whyatt(vertices, epsilon):
     """
-    Line simplification alogorithm
+    Line simplification algorithm
     It takes the coordinates of points representing vertices of a line, eliminates redundant vertices while preserving the shape as much as possible.
     The epsilon parameter defines how many points will be discarded.
 
     Parameters:
-        vertices: list of coordinates of the vertices of the line
-        epsilon: smallest area of a tringle defined by three consecutive points on a line, determines the intensity of simplification
+        vertices (list): list of coordinates of the vertices of the line
+        epsilon (float): smallest area of a triangle defined by three consecutive points on a line, determines the intensity of simplification
 
     Returns:
-        simplified: iterator of tuples; iterates over x-coords and y-coords paralelly
+        simplified (iterator): iterator of tuples; iterates over x-coords and y-coords paralelly
             produces two tuples, one containing x-coords, second containing y-coords of the vertices of the simplified line
     """
 
     # list containing distances between two neighbouring vertices on a line
-    lenghts = []
+    lengths = []
 
     # list containing distances between two vertices on a line skipping one point
     third_sides = []
@@ -49,44 +50,41 @@ def visvalingam_whyatt(vertices, epsilon):
         v1 = vertices[i-1]
         v2 = vertices[i]
         d = pythagoras(*v1, *v2)
-        lenghts.append(d)
+        lengths.append(d)
     for i in range(2, len(vertices)):
         # computes distance between every two vertices on a line skipping one point and adds them to a list
         w1 = vertices[i-2]
         w2 = vertices[i]
         c = pythagoras(*w1, *w2)
         third_sides.append(c)
-    for i in range(1, len(lenghts)):
+    for i in range(1, len(lengths)):
         # computes areas of triangles defined by three consecutive points on a line and adds them to a list
-        a = lenghts[i-1]
-        b = lenghts[i]
+        a = lengths[i-1]
+        b = lengths[i]
         c = third_sides[i-1]
         area = heron(a, b, c)
         areas.append(area)
 
     while len(vertices) > 2 and min(areas) < epsilon:
         """
-        While the line has more than 2 vertices and the smallest area of a triange defined by three consecutive vertices on a line is smaller than epsilon:
+        While the line has more than 2 vertices and the smallest area of a triangle defined by three consecutive vertices on a line is smaller than epsilon:
             finds index of the smallest area of a triangle and eliminates its second point
-            eliminates and recomputes areas and distances that changed due to the removed vertex
+            eliminates and recomputes areas and distances that have changed due to the removed vertex
         """
         rank = areas.index(min(areas))
         vertices.pop(rank+1)
-        lenghts[rank] = third_sides[rank]
-        lenghts.pop(rank+1)
+        lengths[rank] = third_sides[rank]
+        lengths.pop(rank+1)
         if rank != 0:
             # checks if the smallest area of a triangle isn't at the start of the line
-            third_sides[rank -
-                        1] = pythagoras(*vertices[rank-1], *vertices[rank+1])
-            areas[rank-1] = heron(lenghts[rank-1],
-                                  lenghts[rank], third_sides[rank-1])
+            third_sides[rank - 1] = pythagoras(*vertices[rank-1], *vertices[rank+1])
+            areas[rank-1] = heron(lengths[rank-1], lengths[rank], third_sides[rank-1])
         third_sides.pop(rank)
         areas.pop(rank)
         if rank != (len(areas)):
             # checks if the smallest area of a triangle isn't at the end of the line
             third_sides[rank] = pythagoras(*vertices[rank], *vertices[rank+2])
-            areas[rank] = heron(
-                lenghts[rank+1], lenghts[rank], third_sides[rank])
+            areas[rank] = heron(lengths[rank+1], lengths[rank], third_sides[rank])
     simplified = zip(*vertices)
     return simplified
 
@@ -105,7 +103,7 @@ def visualize(raw_x, raw_y, simplified_x, simplified_y):
     ax2.get_yaxis().set_visible(False)
     plt.show()
 
-
+# loads data from the input file
 try:
     with open("input_d1.geojson", encoding="utf-8") as input:
         sample_line = json.load(input)
@@ -117,11 +115,11 @@ except KeyError:
 except:
     sys.exit("Something went wrong.")
 
-# maximum area treshold
-max_area = 0.001
+# minimum area treshold
+epsilon = 0.001
 
 # transposes coordinates of the vertices of the raw line
 before = zip(*vertices)
 
-after = visvalingam_whyatt(vertices, max_area)
+after = visvalingam_whyatt(vertices,epsilon)
 visualize(*before, *after)
